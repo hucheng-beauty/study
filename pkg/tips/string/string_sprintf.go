20dd1b025e50f0c55bd79825f702e877ff3c0a6c
hucheng.jiang <hucheng_beauty@163.com>
2022-02-12 12:13:49 +0800

    feat: string splice

---
+++ pkg/tips/string/string_sprintf.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// StringSprintf SPrintf字符串拼接
+func StringSprintf() {
+	t1 := time.Now().UnixNano() / 1e6
+	fmt.Println("t1 ==> ", t1)
+
+	s := "hello"
+	for i := 0; i < 500000; i++ {
+		s = fmt.Sprintf("%s%s", s, "world")
+	}
+
+	t2 := time.Now().UnixNano() / 1e6
+	fmt.Println("t2 ==> ", t2)
+	fmt.Println("t2 - t1 ==> ", t2-t1)
+}

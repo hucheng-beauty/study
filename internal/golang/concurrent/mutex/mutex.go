8dbf0cb9a88ced8f823271097260209cf4f23dea
hucheng.jiang <hucheng_beauty@163.com>
2022-02-25 18:14:49 +0800

    feat: add the example of mutex and rw_mutex

--- internal/golang/concurrent/mutex/mutex.go
+++ internal/golang/concurrent/mutex/mutex.go
@@ -1,1 +1,31 @@
-package mutex
+package main
+
+import (
+	"log"
+	"sync"
+)
+
+// Counter 计数器
+type Counter struct {
+	mutex sync.Mutex
+	c     int
+}
+
+func (c *Counter) Increase() {
+	c.mutex.Lock()
+	c.c++
+	c.mutex.Unlock()
+}
+
+func NewCounter() *Counter {
+	return &Counter{mutex: sync.Mutex{}, c: 0}
+}
+
+func main() {
+	c := NewCounter()
+	log.Printf("before, counter:%d\n", c.c)
+	for i := 0; i < 10; i++ {
+		c.Increase()
+	}
+	log.Printf("after, counter:%d\n", c.c)
+}
--- internal/golang/concurrent/rw_mutex/rw_mutex.go
+++ internal/golang/concurrent/rw_mutex/rw_mutex.go
@@ -1,1 +1,62 @@
-package rw_mutex
+package main
+
+import (
+	"fmt"
+	"log"
+	"sync"
+)
+
+var secureMapObject *secureMap = NewSecureMap()
+
+type secureMap struct {
+	mutex sync.RWMutex
+	m     map[string]int
+}
+
+func (m *secureMap) Get(key string) (int, bool) {
+	if len(key) <= 0 {
+		return -1, false
+	}
+
+	m.mutex.RLock()
+	defer m.mutex.RUnlock()
+	value, ok := m.m[key]
+	return value, ok
+}
+
+func (m *secureMap) Set(key string, value int) bool {
+	if len(key) <= 0 {
+		log.Println("key invalid")
+		return false
+	}
+
+	_, isExist := m.m[key]
+	if isExist {
+		log.Printf("key is exist,key: %s\n", key)
+	}
+
+	m.mutex.Lock()
+	m.m[key] = value
+	m.mutex.Unlock()
+	return true
+}
+
+func NewSecureMap() *secureMap {
+	return &secureMap{
+		mutex: sync.RWMutex{},
+		m:     make(map[string]int),
+	}
+}
+
+func main() {
+	t := map[string]int{
+		"a": 97,
+		"b": 98,
+		"c": 99,
+	}
+
+	for k, v := range t {
+		secureMapObject.Set(k, v)
+	}
+	fmt.Println("end")
+}
